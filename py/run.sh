01b6b59aec71478d684ba712b6a009e9e46e82c0
Kazuki Onodera <luvsic02@gmail.com>
2018-10-21 14:37:57 +0900

    update

--- py/run.sh
+++ py/run.sh
@@ -1,8 +1,8 @@
 
-python -u 000.py
-python -u 001.py
-python -u 002_aggregate.py
-python -u 003_aggregate_diff.py
-python -u 004_aggregate_pb.py
-python -u 005_aggregate_pb.py
+python -u 000.py > LOG/log_000.py.txt &
+python -u 001.py > LOG/log_001.py.txt &
+python -u 002_aggregate.py > LOG/log_002_aggregate.py.txt &
+python -u 003_aggregate_diff.py > LOG/log_003_aggregate_diff.py.txt &
+python -u 004_aggregate_pb.py > LOG/log_004_aggregate_pb.py.txt &
+python -u 005_aggregate_pb.py > LOG/log_005_aggregate_pb.py.txt &
 
