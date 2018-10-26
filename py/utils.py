2b6ab0dd17b78fc5f2b3abcf25cc777327d10ded
kazuki-onodera <kazuki.onodera@dena.com>
2018-10-26 17:46:32 +0900

    update

--- py/utils.py
+++ py/utils.py
@@ -254,40 +254,40 @@ def lgb_multi_weighted_logloss_exgal(y_preds, train_data):
     loss = - np.sum(y_w) / np.sum(class_arr)
     return 'wloss', loss, False
 
-def lgb_multi_weighted_logloss(y_preds, train_data):
-    """
-    @author olivier https://www.kaggle.com/ogrellier
-    https://www.kaggle.com/ogrellier/plasticc-in-a-kernel-meta-and-data/code
-    multi logloss for PLAsTiCC challenge
-    """
-    # class_weights taken from Giba's topic : https://www.kaggle.com/titericz
-    # https://www.kaggle.com/c/PLAsTiCC-2018/discussion/67194
-    # with Kyle Boone's post https://www.kaggle.com/kyleboone
-    y_true = train_data.get_label()
-    if len(np.unique(y_true)) > 14:
-        classes.append(99)
-        class_weight[99] = 2
-    
-    y_p = y_preds.reshape(y_true.shape[0], len(classes), order='F')
-
-    # Trasform y_true in dummies
-    y_ohe = pd.get_dummies(y_true)
-    # Normalize rows and limit y_preds to 1e-15, 1-1e-15
-    y_p = np.clip(a=y_p, a_min=1e-15, a_max=1 - 1e-15)
-    # Transform to log
-    y_p_log = np.log(y_p)
-    # Get the log for ones, .values is used to drop the index of DataFrames
-    # Exclude class 99 for now, since there is no class99 in the training set
-    # we gave a special process for that class
-    y_log_ones = np.sum(y_ohe.values * y_p_log, axis=0)
-    # Get the number of positives for each class
-    nb_pos = y_ohe.sum(axis=0).values.astype(float)
-    # Weight average and divide by the number of positives
-    class_arr = np.array([class_weight[k] for k in sorted(class_weight.keys())])
-    y_w = y_log_ones * class_arr / nb_pos
-
-    loss = - np.sum(y_w) / np.sum(class_arr)
-    return 'wloss', loss, False
+#def lgb_multi_weighted_logloss(y_preds, train_data):
+#    """
+#    @author olivier https://www.kaggle.com/ogrellier
+#    https://www.kaggle.com/ogrellier/plasticc-in-a-kernel-meta-and-data/code
+#    multi logloss for PLAsTiCC challenge
+#    """
+#    # class_weights taken from Giba's topic : https://www.kaggle.com/titericz
+#    # https://www.kaggle.com/c/PLAsTiCC-2018/discussion/67194
+#    # with Kyle Boone's post https://www.kaggle.com/kyleboone
+#    y_true = train_data.get_label()
+#    if len(np.unique(y_true)) > 14:
+#        classes.append(99)
+#        class_weight[99] = 2
+#    
+#    y_p = y_preds.reshape(y_true.shape[0], len(classes), order='F')
+#
+#    # Trasform y_true in dummies
+#    y_ohe = pd.get_dummies(y_true)
+#    # Normalize rows and limit y_preds to 1e-15, 1-1e-15
+#    y_p = np.clip(a=y_p, a_min=1e-15, a_max=1 - 1e-15)
+#    # Transform to log
+#    y_p_log = np.log(y_p)
+#    # Get the log for ones, .values is used to drop the index of DataFrames
+#    # Exclude class 99 for now, since there is no class99 in the training set
+#    # we gave a special process for that class
+#    y_log_ones = np.sum(y_ohe.values * y_p_log, axis=0)
+#    # Get the number of positives for each class
+#    nb_pos = y_ohe.sum(axis=0).values.astype(float)
+#    # Weight average and divide by the number of positives
+#    class_arr = np.array([class_weight[k] for k in sorted(class_weight.keys())])
+#    y_w = y_log_ones * class_arr / nb_pos
+#
+#    loss = - np.sum(y_w) / np.sum(class_arr)
+#    return 'wloss', loss, False
 
 
 def load_train(col=None):
