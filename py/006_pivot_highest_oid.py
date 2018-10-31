06198b31c346e6b528c16922f760b5171a44f957
kazuki-onodera <kazuki.onodera@dena.com>
2018-10-31 21:16:51 +0900

    update

---
+++ py/006_pivot_highest_oid.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 21:00:17 2018
+
+@author: kazuki.onodera
+
+focus on highest year
+
+keys: object_id
+
+
+"""
+
+import numpy as np
+import pandas as pd
+import os
+from glob import glob
+from multiprocessing import cpu_count, Pool
+import utils
+
+PREF = 'f006'
+
+os.system(f'rm ../data/t*_{PREF}*')
+os.system(f'rm ../feature/t*_{PREF}*')
+
+def quantile(n):
+    def quantile_(x):
+        return np.percentile(x, n)
+    quantile_.__name__ = 'q%s' % n
+    return quantile_
+
+num_aggregations = {
+#    'mjd':      ['min', 'max', 'size'],
+#    'passband': ['min', 'max', 'mean', 'median', 'std', quantile(25), quantile(75)],
+    'flux':        ['min', 'max', 'mean', 'median', 'std', quantile(25), quantile(75)],
+    'flux_norm1':  ['min', 'max', 'mean', 'median', 'std', quantile(25), quantile(75)],
+    'flux_norm2':  ['min', 'max', 'mean', 'median', 'std', quantile(25), quantile(75)],
+    'flux_err':    ['min', 'max', 'mean', 'median', 'std', quantile(25), quantile(75)],
+    'detected':    ['min', 'max', 'mean', 'median', 'std', quantile(25), quantile(75)],
+    }
+
+def aggregate(df, output_path, drop_oid=True):
+    """
+    df = pd.read_pickle('../data/train_log.pkl')
+    """
+    
+    idxmax = df.groupby('object_id').flux.idxmax()
+    keep = df.iloc[idxmax][['object_id', 'year']]
+    df = pd.merge(keep, df, on=['object_id', 'year'], how='left')
+    
+    pt = pd.pivot_table(df, index=['object_id'], 
+                        aggfunc=num_aggregations)
+    
+    pt.columns = pd.Index([f'{e[0]}_{e[1]}' for e in pt.columns.tolist()])
+    
+    # std / mean
+    col_std = [c for c in pt.columns if c.endswith('_std')]
+    for c in col_std:
+        pt[f'{c}-d-mean'] = pt[c]/pt[c.replace('_std', '_mean')]
+    
+    # max / min, max - min
+    col_max = [c for c in pt.columns if c.endswith('_max')]
+    for c in col_max:
+        pt[f'{c}-d-min'] = pt[c]/pt[c.replace('_max', '_min')]
+        pt[f'{c}-m-min'] = pt[c]-pt[c.replace('_max', '_min')]
+    
+    if drop_oid:
+        pt.reset_index(drop=True, inplace=True)
+    else:
+        pt.reset_index(inplace=True)
+    pt.add_prefix(PREF+'_').to_pickle(output_path)
+    
+    return
+
+def multi(args):
+    input_path, output_path = args
+    aggregate(pd.read_pickle(input_path), output_path, drop_oid=False)
+    return
+
+# =============================================================================
+# main
+# =============================================================================
+if __name__ == "__main__":
+    utils.start(__file__)
+    
+    aggregate(pd.read_pickle('../data/train_log.pkl'), f'../data/train_{PREF}.pkl')
+    
+    # test
+    os.system(f'rm ../data/tmp_{PREF}*')
+    argss = []
+    for i,file in enumerate(utils.TEST_LOGS):
+        argss.append([file, f'../data/tmp_{PREF}{i}.pkl'])
+    pool = Pool( cpu_count() )
+    pool.map(multi, argss)
+    pool.close()
+    df = pd.concat([pd.read_pickle(f) for f in glob(f'../data/tmp_{PREF}*')], 
+                    ignore_index=True)
+    df.sort_values(f'{PREF}_object_id', inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    del df[f'{PREF}_object_id']
+    df.to_pickle(f'../data/test_{PREF}.pkl')
+    os.system(f'rm ../data/tmp_{PREF}*')
+    
+    utils.end(__file__)
+
+
