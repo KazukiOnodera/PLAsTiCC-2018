b9a5da5e99d56dceac0a575dcf5ddf2a9c45657d
Kazuki Onodera <luvsic02@gmail.com>
2018-10-31 10:18:50 +0900

    update

---
+++ py/weighted_logloss.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 09:18:46 2018
+
+@author: Kazuki
+"""
+
+import numpy as np
+import pandas as pd
+
+
+y_true = np.random.rand(100, 10)
+
+argmax = y_true.argmax(1)
+for i,e in enumerate(argmax):
+    y_true[i] = 0
+    y_true[i, e] = 1
+
+
+weights = np.random.uniform(size=10)
+
+
+y_pred = np.random.rand(100, 10)
+sum1 = y_pred.sum(1)
+for i in range(10):
+    y_pred[:,i] /= sum1
+
+
+
+logloss = 0
+M = len(weights)
+N = y_true.shape[0]
+for i in range(M):
+    tmp = 0
+    w = weights[i]
+    for j in range(N):
+        tmp += (y_true[j,i] * np.log(y_pred[j,i]))
+    logloss += w * tmp
+logloss /= -sum(weights)
+
+
