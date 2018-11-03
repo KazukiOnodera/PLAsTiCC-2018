ff36d25f9db61be02648b67df3683f93d5f3d8de
Kazuki Onodera <luvsic02@gmail.com>
2018-11-04 00:08:42 +0900

    update

--- py/run.sh
+++ py/run.sh
@@ -1,9 +1,5 @@
 
 # nohup sh run.sh &
-python -u 000.py > LOG/log_000.py.txt
-python -u 001.py > LOG/log_001.py.txt
-python -u 002_aggregate.py > LOG/log_002_aggregate.py.txt
-python -u 003_aggregate_diff.py > LOG/log_003_aggregate_diff.py.txt
-python -u 004_aggregate_pb.py > LOG/log_004_aggregate_pb.py.txt
-python -u 005_aggregate_pb.py > LOG/log_005_aggregate_pb.py.txt
+python -u 801_cv.py > LOG/log_801_cv.py.txt
+python -u 916_predict_1103-2.py > LOG/log_916_predict_1103-2.py.txt
 
