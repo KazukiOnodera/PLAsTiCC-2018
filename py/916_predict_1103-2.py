781ec5d7058ec2819c6b25ae396d9e4e50842cca
Kazuki Onodera <luvsic02@gmail.com>
2018-11-04 21:00:43 +0900

    update

--- py/916_predict_1103-2.py
+++ py/916_predict_1103-2.py
@@ -107,6 +107,7 @@ gc.collect()
 model_all = []
 nround_mean = 0
 wloss_list = []
+y_preds = []
 for i in range(LOOP):
     gc.collect()
     param['seed'] = np.random.randint(9999)
@@ -114,6 +115,9 @@ for i in range(LOOP):
                          feval=utils.lgb_multi_weighted_logloss,
                          early_stopping_rounds=100, verbose_eval=50,
                          seed=SEED)
+    y_pred = ex.eval_oob(X, y, models, SEED, stratified=True, shuffle=True, 
+                         n_class=y.unique().shape[0])
+    y_preds.append(y_pred)
     model_all += models
     nround_mean += len(ret['multi_logloss-mean'])
     wloss_list.append( ret['wloss-mean'][-1] )
@@ -137,6 +141,19 @@ imp.to_csv(f'LOG/imp_{__file__}.csv', index=False)
 png = f'LOG/imp_{__file__}.png'
 utils.savefig_imp(imp, png, x='total', title=f'{__file__}')
 utils.send_line(result, png)
+
+
+# =============================================================================
+# weight
+# =============================================================================
+import utils_post
+
+tmp = np.zeros(y_pred.shape)
+for y_pred in y_preds:
+    tmp += y_pred
+tmp /= len(y_preds)
+
+y_true = pd.get_dummies(y)
 
 
 # =============================================================================
---
+++ py/utils_post.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  4 20:25:09 2018
+
+@author: Kazuki
+"""
+
+import numpy as np
+import pandas as pd
+#import os
+#from multiprocessing import cpu_count, Pool
+#import utils
+
+
+M = 15
+
+def multi_weighted_logloss(y_true, y_pred, myweight=None):
+    """
+    @author olivier https://www.kaggle.com/ogrellier
+    multi logloss for PLAsTiCC challenge
+    """
+    # class_weights taken from Giba's topic : https://www.kaggle.com/titericz
+    # https://www.kaggle.com/c/PLAsTiCC-2018/discussion/67194
+    # with Kyle Boone's post https://www.kaggle.com/kyleboone
+    classes = [6, 15, 16, 42, 52, 53, 62, 64, 65, 67, 88, 90, 92, 95]
+    class_weight = {6: 1, 15: 2, 16: 1, 42: 1, 52: 1, 53: 1, 62: 1, 64: 2, 65: 1, 67: 1, 88: 1, 90: 1, 92: 1, 95: 1}
+    if y_true.shape[1] > 14:
+        classes.append(99)
+        class_weight[99] = 2
+    
+    if myweight is None:
+        myweight = np.ones(M)
+    y_p = y_pred * myweight
+    
+    # normalize
+    y_p /= y_p.sum(1)[:,None]
+    
+    # Normalize rows and limit y_preds to 1e-15, 1-1e-15
+    y_p = np.clip(a=y_p, a_min=1e-15, a_max=1 - 1e-15)
+    # Transform to log
+    y_p_log = np.log(y_p)
+    # Get the log for ones, .values is used to drop the index of DataFrames
+    # Exclude class 99 for now, since there is no class99 in the training set
+    # we gave a special process for that class
+    y_log_ones = np.sum(y_true * y_p_log, axis=0)
+    # Get the number of positives for each class
+    nb_pos = y_true.sum(axis=0).astype(float)
+    # Weight average and divide by the number of positives
+    class_arr = np.array([class_weight[k] for k in sorted(class_weight.keys())])
+    y_w = y_log_ones * class_arr / nb_pos
+
+    loss = - np.nansum(y_w) / np.sum(class_arr)
+    return loss
+
+
+def calc_gradient(f, X):
+    """
+    calc_gradient
+    偏微分を行う関数
+    関数fを変数xの各要素で偏微分した結果をベクトルにした勾配を返す
+    
+    @params
+    f: 対象となる関数
+    X: 関数fの引数のベクトル(numpy.array)
+    
+    @return
+    gradient: 勾配(numpy.array)
+    """
+    
+    h = 1e-4
+    gradient = np.zeros_like(X)
+    
+    # 各変数についての偏微分を計算する
+    for i in range(X.size):
+        store_X = X[:]
+        
+        # f(x+h)
+        X[i] += h
+        f_x_plus_h = f(X)
+
+        X = store_X[:]
+        
+        # f(x-h)
+        X[i] -= h
+        f_x_minus_h = f(X)
+        
+        # 偏微分
+        gradient[i] = (f_x_plus_h - f_x_minus_h) / (2 * h)
+        
+    return gradient
+
+def gradient_descent(f, X, learning_rate, max_iter, is_print=False):
+    """
+    gradient_descent
+    最急降下法を行う関数
+    
+    @params
+    f: 対象となる関数
+    X: 関数fの引数のベクトル(numpy.array)
+    learning_rate: 学習率
+    max_iter: 繰り返し回数
+    
+    @return
+    X: 関数の出力を最小にする(であろう)引数(numpy.array)
+    """
+    score_bk = 9999
+    for i in range(max_iter):
+        X -= (learning_rate * calc_gradient(f, X))
+        score = f(X)
+        if score_bk < score:
+            break
+        score_bk = score
+        if is_print and i%50==0:
+            print("[{:3d}] X = {}, f(X) = {:.7f}".format(i, X, score))
+        
+    return X
+
+def get_weight(y_true, y_pred, weight=None, eta=1, nround=100):
+    M = y_true.shape[1]
+    if weight is None:
+        weight = np.ones(M)
+    f = lambda X: multi_weighted_logloss(y_true, y_pred, weight)
+    gradient_descent(f, weight, learning_rate=eta, max_iter=nround)
+    return weight
+
+
+
+
