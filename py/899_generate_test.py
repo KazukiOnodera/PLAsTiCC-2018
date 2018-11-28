819cc0e61174160ca1b761e19af87a7974419bf0
kazuki-onodera <kazuki.onodera@dena.com>
2018-11-28 17:00:28 +0900

    update

--- py/899_generate_test.py
+++ py/899_generate_test.py
@@ -21,7 +21,7 @@ NTHREADS = 2
 
 
 
-COL = pd.read_csv('LOG/imp_808_cv.py-2.csv').head(NFEATURES).feature.tolist()
+COL = pd.read_csv('LOG/imp_801_cv.py-2.csv').head(NFEATURES).feature.tolist()
 
 #COL_gal   = pd.read_csv('LOG/imp_802_cv_separate.py_gal.csv').head(NFEATURES ).feature.tolist()
 #COL_exgal = pd.read_csv('LOG/imp_802_cv_separate.py_exgal.csv').head(NFEATURES ).feature.tolist()
