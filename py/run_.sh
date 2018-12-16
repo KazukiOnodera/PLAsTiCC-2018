2556bd762bb2d25513a4892d1f4acfcd6c0f2420
Kazuki Onodera <luvsic02@gmail.com>
2018-12-16 18:20:52 +0900

    update

---
+++ py/run_.sh
@@ -0,0 +1,10 @@
+# nohup sh run_.sh &
+
+nohup python -u 962_predict_mlogloss_1216-1.py > LOG/log_962_predict_mlogloss_1216-1.py.txt &
+nohup python -u 962_predict_mlogloss_1216-2.py > LOG/log_962_predict_mlogloss_1216-2.py.txt &
+nohup python -u 962_predict_mlogloss_1216-3.py > LOG/log_962_predict_mlogloss_1216-3.py.txt &
+
+nohup python -u 963_predict_wmlogloss_1216-1.py > LOG/log_963_predict_wmlogloss_1216-1.py.txt &
+nohup python -u 963_predict_wmlogloss_1216-2.py > LOG/log_963_predict_wmlogloss_1216-2.py.txt &
+nohup python -u 963_predict_wmlogloss_1216-3.py > LOG/log_963_predict_wmlogloss_1216-3.py.txt &
+
