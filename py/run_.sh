3bae840e4151b2a0454f78da77e50430ad45d85b
Kazuki Onodera <luvsic02@gmail.com>
2018-12-16 18:26:04 +0900

    update

--- py/run_.sh
+++ py/run_.sh
@@ -1,10 +1,10 @@
 # nohup sh run_.sh &
 
-python -u 962_predict_mlogloss_1216-1.py > LOG/log_962_predict_mlogloss_1216-1.py.txt &
-python -u 962_predict_mlogloss_1216-2.py > LOG/log_962_predict_mlogloss_1216-2.py.txt &
-python -u 962_predict_mlogloss_1216-3.py > LOG/log_962_predict_mlogloss_1216-3.py.txt &
+python -u 962_predict_mlogloss_1216-1.py > LOG/log_962_predict_mlogloss_1216-1.py.txt
+python -u 962_predict_mlogloss_1216-2.py > LOG/log_962_predict_mlogloss_1216-2.py.txt
+python -u 962_predict_mlogloss_1216-3.py > LOG/log_962_predict_mlogloss_1216-3.py.txt
 
-python -u 963_predict_wmlogloss_1216-1.py > LOG/log_963_predict_wmlogloss_1216-1.py.txt &
-python -u 963_predict_wmlogloss_1216-2.py > LOG/log_963_predict_wmlogloss_1216-2.py.txt &
-python -u 963_predict_wmlogloss_1216-3.py > LOG/log_963_predict_wmlogloss_1216-3.py.txt &
+python -u 963_predict_wmlogloss_1216-1.py > LOG/log_963_predict_wmlogloss_1216-1.py.txt
+python -u 963_predict_wmlogloss_1216-2.py > LOG/log_963_predict_wmlogloss_1216-2.py.txt
+python -u 963_predict_wmlogloss_1216-3.py > LOG/log_963_predict_wmlogloss_1216-3.py.txt
 
